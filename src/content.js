ee3a0b2f963bf4645614196b147b1f43671a9116
Vladimir Kalnitsky <klntsky@gmail.com>
2023-09-10 20:36:28 +0400

    Clean up the implementation

--- src/content.js
+++ src/content.js
@@ -1,32 +1,64 @@
 (() => {
+    // counter for unique animation frame request IDs
     let counter = 0;
-    let fps = 0;
-    const original = window._unlagify_real_requestAnimationFrame = window['requestAnimationFrame'];
-    let requests = [];
 
+    // FPS counter
+    let fps = 0;
+    const originalRequestAnimationFrame = window._unlagify_real_requestAnimationFrame = window['requestAnimationFrame'];
+
+    // A map that contains animation frame requests. IDs are keys.
+    let requests = new Map();
+
+    // A new event loop implementation
     const processRequests = () => {
         const reqsCountAtStart = requests.length;
-        console.log('processRequests', requests.length);
         const timeNow = performance.now();
 
-        for (let value of requests.slice()) {
+        for (let [key, value] of Array.from(requests.entries())) {
             try {
+                // process the request
                 value(timeNow);
             } catch (e) {
-                console.log(e);
+                // TODO: rethrow properly
+                console.error(e);
             } finally {
+                requests.delete(key);
             }
         }
-
-        requests = requests.slice(reqsCountAtStart);
-        // original(processRequests);
     };
 
     requestAnimationFrame = (callback) => {
-        console.log('requestAnimationFrame');
-        requests.push(callback);
-        // original(processRequests);
+        requests.set(counter++, callback);
+    };
+
+    cancelAnimationFrame = id => {
+        requests.delete(id);
     };
 
     setInterval(processRequests, 300);
+
+    {
+        let lastFPS = 0;
+        let lastTime = performance.now();
+
+        const updateFPS = () => {
+            const nowTime = performance.now();
+            if (nowTime - lastTime >= 1000) {
+                fps = lastFPS;
+            lastFPS = 0;
+                lastTime = nowTime;
+                console.log('fps', fps);
+            } else {
+                lastFPS++;
+            }
+        };
+
+        const updateFPSLoop = () => {
+            updateFPS();
+            originalRequestAnimationFrame(updateFPSLoop);
+        };
+
+        updateFPSLoop();
+    }
+
 })();
